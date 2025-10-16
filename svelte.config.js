523cc159253f64ad77f345bd9a1139a5572c416a
David Monaghan <monaghan.david@gmail.com>
2025-10-16 07:54:28 -0400

    external: ["@node-rs/argon2-wasm32-wasi"]

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from "@sveltejs/adapter-cloudflare";
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,8 +11,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		vite: {
+			build: {
+				external: ["@node-rs/argon2-wasm32-wasi"],
+			},
+		},
+		adapter: adapter(),
+	},
 };
 
 export default config;
